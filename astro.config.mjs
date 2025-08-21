6287b259aa79388ac33735dd3eecef4d6a5a3dad
Dusty McCord <dustymccord@gmail.com>
2025-08-20 19:05:38 -0700

    add decap

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://dustatron.github.io",
+  // Only use base path for production builds, not for development
+  base: process.env.NODE_ENV === "production" ? "/dustatron-site" : "",
+  outDir: "./dist",
+  publicDir: "./public",
+  build: {
+    assets: "_assets",
+  },
+});
