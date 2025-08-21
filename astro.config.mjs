14cd074aa7d9b53404d361982a26d6e83d81517c
Dusty McCord <dustymccord@gmail.com>
2025-08-20 20:19:44 -0700

    yet more configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,9 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://dustatron.github.io/dustatron-site/",
+  site: "https://dustatron.github.io",
   // Only use base path for production builds, not for development
-  base: process.env.NODE_ENV === "production" ? "/" : "",
+  base: process.env.NODE_ENV === "production" ? "/dustatron-site" : "",
   outDir: "./dist",
   publicDir: "./public",
   build: {
